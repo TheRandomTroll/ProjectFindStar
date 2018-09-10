956622667583ac7fe6b4f06f46a1cc5cbd8f61ab
Mihail Gerginov <mihailgerginov01@gmail.com>
2018-09-10 14:46:15 +0300

    Added Admin Area
    Added the Admin area and attached CSS and JS to it.

--- ProjectFindStar/Startup.cs
+++ ProjectFindStar/Startup.cs
@@ -68,6 +68,9 @@ namespace ProjectFindStar
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
